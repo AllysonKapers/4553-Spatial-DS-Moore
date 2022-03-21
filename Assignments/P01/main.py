863a40631ab89d2ce169217f58512a3dce3f1fb8
Ally Moore <ally_moore6@hotmail.com>
2022-03-20 20:09:58 -0500

    revision

--- Assignments/P01/main.py
+++ Assignments/P01/main.py
@@ -1,20 +1,11 @@
 import json
 import random
-<<<<<<< HEAD
-## returns a random color
-=======
 
-##returns a randon color for point marker
->>>>>>> 25859b5a7d82b26bc0bf691df586562ad3d21ebc
 def randColor():
   r = lambda: random.randint(0,255)
   return ('#%02X%02X%02X' % (r(),r(),r()))
 
-<<<<<<< HEAD
-##function to make a point
-=======
-##creates a point on the map
->>>>>>> 25859b5a7d82b26bc0bf691df586562ad3d21ebc
+
 def makePoint(city, i):
   feature = {
     "type": "Feature",
@@ -38,15 +29,15 @@ def makePoint(city, i):
       feature['properties'][key] = val
 
   return feature
-
   
 
 # Change path as appropriate
-with open('cities_latlon_w_pop.json') as f:
+with open("/Users/AllyMoore/Documents/GitHub/4553-Spatial-DS-Moore/Assignments/P01/cities_latlon_w_pop.json") as f:
   data = json.load(f)
 
 #will hold an value for each state without duplicates
 states = {}
+
 
 #iterates through the entire json file
 for item in data:
@@ -96,39 +87,20 @@ for spot in lng:
         if states[item]['longitude'] == spot:
             points.append(states[item])
 
-<<<<<<< HEAD
-##creates Feature Collection for geoJson file
-=======
-##creates FeatureCollection for geoJson
->>>>>>> 25859b5a7d82b26bc0bf691df586562ad3d21ebc
 FeatureCollection = {
     "type" : "FeatureCollection",
     "features" : []
 }
-<<<<<<< HEAD
-##interates through points list
 for i in range(len(points)):
-    ##uses makePoint function to add points to Feature Collection
     FeatureCollection["features"].append(makePoint(points[i], i))
-    ##'draws' lines between points to create a path through the points
-=======
-
-##iterates through points
-for i in range(len(points)):
-    ##adds a point for each index in points
-    FeatureCollection["features"].append(makePoint(points[i], i))
-    ##adds lines between points
->>>>>>> 25859b5a7d82b26bc0bf691df586562ad3d21ebc
     if i != len(points) -1:
         FeatureCollection['features'].append(
             {
                 "type":"Feature",
-                ##controls line properties
                 "properties":{
                     "lineColor": '#000000',
                     "LineWidth" : 3
                 },
-                ##draws a line between coordinates
                 "geometry": {
                     "type": "LineString",
                     "coordinates": [
@@ -138,11 +110,6 @@ for i in range(len(points)):
                 }
             }
         )
-<<<<<<< HEAD
-##opens output file and writes Feature Collection to it 
-=======
 
-##opens geoJson file for writing
->>>>>>> 25859b5a7d82b26bc0bf691df586562ad3d21ebc
 with open("Path.geojson","w") as f:
   f.write(json.dumps(FeatureCollection))
